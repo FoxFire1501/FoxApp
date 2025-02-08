a580f8da7c1e65e6b0e9d3640576472891d719e2
DuckyDevVN <anhhvtk00394@gmail.com>
2025-02-08 09:03:18 +0700

    event welcome

--- events/guilds/memberAdd.ts
+++ events/guilds/memberAdd.ts
@@ -1,12 +1,27 @@
-import { Event } from "modules";
-import { Events, GuildMember } from "discord.js";
+import { createWelImage, Event, newLog } from "modules";
+import { Events, GuildMember, TextChannel } from "discord.js";
 import ClientBase from "index";
 
 async function memberAdd(member: GuildMember) {
     const client = member.client as ClientBase;
-    const guiWelChannel = await client.db?.get(`${member.guild.id}.welChannelId`)
+    const guiWelChannelID = await client.db?.get(`${member.guild.id}.welChannelId`) as string
 
-    if (!guiWelChannel) return;
+    if (!guiWelChannelID) return;
+
+    const guiWelChannel = await member.guild.channels.cache.get(guiWelChannelID)?.fetch() as TextChannel
+
+    try {
+        guiWelChannel.send({
+            files: [
+                {
+                    attachment: await createWelImage(member),
+                    name: `welcome_${member.id}.png`
+                }
+            ]
+        })
+    } catch (err) {
+        newLog(err, "error")
+    }
 }
 
 export default new Event({
